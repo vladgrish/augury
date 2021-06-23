6ec6d6ee72445ba61ac8c50b56bc867b78681e0f
vladgrish <vladislav.grischinsky@gmail.com>
2021-06-23 22:08:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from Aggregator import Aggregator
 
 if __name__ == "__main__":
     """
-    Here we simply loop over all of the machines in the queue.
-    The assamption is that we are able to fullfil the task in under an hour
-    Otherwise a different approach is needed. maybe async functionality to read\write from fs
+    Here we simply loop over all of the machines and their queues one by one.
+    The assumption is that we are able to fullfil the task in under an hour
+    Otherwise a different approach is needed. maybe async functionality to read\write from fs or a priority queue
     
     *given more time, I would probably use a more sophisticated method to manage the queue
     and also would wrap Aggregator and Queue Manager with FastAPI and put into separate containers
